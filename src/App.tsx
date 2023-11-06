171da52d65406cf77f067728f61454b144659f91
Joshua(노경환) <khnoh@carrotins.com>
2023-11-07 08:53:56 +0900

    d

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        testtesttest
+        testtesttest222
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
